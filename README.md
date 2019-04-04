ba239556c9898713739ba9f5899a18714e5f787f
Claudio Spiess <claudiosv@users.noreply.github.com>
2019-04-04 21:08:22 +0200

    Add version detection

--- README.md
+++ README.md
@@ -13,6 +13,30 @@ TODO: Write usage instructions here
 ## Development
 
 TODO: Write development instructions here
+Make sure you have crystal & openssl installed. For MacOs, run
+```
+brew install crystal openssl
+```
+
+It is critical to compile this project, that you add the following lines to your bash_profile or zshrc:
+```
+export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig:$PKG_CONFIG_PATH"
+export LDFLAGS="-L/usr/local/opt/libffi/lib -L/usr/local/opt/openssl/lib -L/usr/local/opt/llvm@6/lib -Wl,-rpath,/usr/local/opt/llvm@6/lib -L/usr/local/opt/llvm@6/lib $LDFLAGS"
+export CPPFLAGS="-I/usr/local/opt/openssl/include -I/usr/local/opt/llvm@6/include $CPPFLAGS"
+```
+
+This adds LLVM, LibFFI, and OpenSSL to your compiler flags.
+
+Now you can run this project (this will compile the project, hence the long startup time): 
+```
+crystal run src/toodlemoodle.cr
+```
+
+You can also build an executable:
+```
+crystal build src/toodlemoodle.cr
+```
+Use the  --release flag for an optimized production build.
 
 ## Contributing
 
--- src/toodlemoodle.cr
+++ src/toodlemoodle.cr
@@ -1,6 +1,41 @@
 # TODO: Write documentation for `Toodlemoodle`
+require "http/client"
+require "option_parser"
+
 module Toodlemoodle
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  scan = false
+  target = Nil
+
+  OptionParser.parse! do |parser|
+    parser.banner = "Usage: tmoodle [arguments] TARGET"
+    parser.on("-s", "--scan", "Scans the target (default)") { scan = true }
+    parser.on("-t NAME", "--to=NAME", "Specifies the name to target (ignore, for ref)") { |name| destination = name }
+    parser.on("-h", "--help", "Show this help") { puts parser }
+    parser.invalid_option do |flag|
+      STDERR.puts "ERROR: #{flag} is not a valid option.\n"
+      STDERR.puts parser
+      exit(1)
+    end
+    parser.unknown_args do |args|
+      target = args[0]
+    end
+  end
+
+  if !target
+    STDERR.puts "ERROR: Please specify a target.\n"
+    exit(1)
+  else
+    puts "Attacking #{target}!"
+    version_regex = /(?:(\d+)\.)?(?:(\d+)\.)?(\d+)/
+
+    # TODO: Put your code here
+    params = HTTP::Params.encode({"author" => "John Doe", "offset" => "20"}) # => author=John+Doe&offset=20
+    response = HTTP::Client.get "https://#{target}/lib/upgrade.txt"          # https://ole.unibz.it
+    # puts response.body
+    if regex_data = response.body.match(version_regex)
+      puts "Version detected: " + regex_data.not_nil![0] # => 200
+    end
+  end
 end

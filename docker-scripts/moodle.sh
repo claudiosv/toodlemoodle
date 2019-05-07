648888f2ec5ec6810d57eea5d4c430f9edd46532
Riccardo Felluga <riki.1@hotmail.it>
2019-05-07 15:31:42 +0200

    Separated session info extractor from rce_exploit

---
+++ docker-scripts/moodle.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+set -e
+#some vars
+export MOODLE_DOCKER_WWWROOT=./moodle
+export MOODLE_VERSION=3.1.13
+export MOODLE_DOCKER_DB=mariadb
+export MOODLE_DOCKER_PHP_VERSION=7.1
+export MOODLE_DOCKER_WEB_HOST=localhost
+export MOODLE_DOCKER_WEB_PORT=8000
+export ASSETDIR=./assets
+
+function all() {
+	kill_containers
+	build
+	run
+}
+function clean() {
+	echo "[-] THIS WILL REMOVE THE PREVIOUS MOODLE INSTALLATION!(if present)"
+	read -p "Are you sure? [yN]" -n 1 -r
+	echo # (optional) move to a new line
+	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+		echo "Quitting..."
+		[[ "$0" == "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+	fi
+	if [ -f "./tmp.zip" ]; then
+		rm ./tmp.zip
+	fi
+	if [ -d "./moodle" ]; then
+		rm -rf ./moodle
+	fi
+	if [ -d "./assets" ]; then
+		rm -rf ./assets
+	fi
+	docker-compose -f base-moodle.yml rm webserver db
+}
+function build() {
+	echo "[+] Downloading Moodle VERSION $MOODLE_VERSION"
+	clean
+	wget -O- -O ./tmp.zip https://downloads.sourceforge.net/project/moodle/Moodle/stable32/moodle-$MOODLE_VERSION.zip
+	if [! -f "./tmp.zip" ]; then
+		echo "Problems downloading the moodle"
+	fi
+	unzip ./tmp.zip -d ./
+	rm ./tmp.zip
+	mkdir ./assets
+	cp ./config.docker-template.php $MOODLE_DOCKER_WWWROOT/config.php
+}
+function exec_statement() {
+	dockercompose="docker-compose -f ./base-moodle.yml"
+	# Mac OS Compatbility
+	if [[ "$(uname)" == "Darwin" ]]; then
+		# Support https://docs.docker.com/docker-for-mac/osxfs-caching/
+		dockercompose="${dockercompose} -f ./volumes-cached.yml"
+	fi
+	$dockercompose $1
+}
+function run(){
+	exec_statement up
+}
+function kill_containers() {
+	exec_statement kill
+	# docker stop $(docker ps -q)
+	# docker rm $(docker ps -a -q)
+	# docker rmi $(docker images -q)
+	# docker system prune
+}
+while test $# -gt 0; do
+	echo "[*] Options: clean, build, run, kill or all"
+	case $1 in
+	all)
+		all
+		;;
+	clean)
+		clean
+		;;
+	build)
+		build
+		;;
+	run)
+		run
+		;;
+	kill)
+		kill_containers
+		;;
+	*) echo "Not an option!" ;;
+	esac
+	break
+done
---
+++ src/actions/SessionInfo.cr
@@ -0,0 +1,61 @@
+# TODO: Write documentation for `Toodlemoodle`
+require "http/client"
+require "option_parser"
+require "json"
+require "base64"
+
+class SessionInfo
+  def perform(target)
+    puts "[?] Enter the username"
+    username = gets().not_nil!
+    puts "[?] Enter the password"
+    psw = gets().not_nil!
+    session = get_session(target, username, psw)
+    puts "[*] Session token: " + session["moodle_session"]
+    puts "[*] Session key: " + session["session_key"]
+    puts "[!] Done, session retrived!"
+  end
+
+  def get_session(url, user, psw)
+    data = {
+      "anchor"   => "",
+      "username" => user,
+      "password" => psw,
+    }
+    puts "[>] Logging in as " + data["username"]
+    redirect_response = http_post(url + "/login/index.php", data)
+
+    if redirect_response.status_code == 303
+      # extract the new moodle session ID
+      moodle_session = /path=\/\", \"MoodleSession=(.*); path=\/\",/.match(redirect_response.headers.to_s).try &.[1]
+      puts "[>] Extracting new session ID"
+      response = http_get(url + "/my/", moodle_session)
+      if response.status_code == 303
+        puts "[!] Login failed!"
+      else
+        session_key = /sesskey\":\"(.*)\",\"loadingicon/.match(response.body).try &.[1]
+        puts "[>] Authenticated session ID => " + moodle_session.not_nil!
+        puts "[>] Authenticated session key => " + session_key.not_nil!
+      end
+    end
+    return {"moodle_session" => moodle_session.not_nil!, "session_key" => session_key.not_nil!}
+  end
+
+  private def http_post(url, data : String | Hash(String, String | Int32), moodle_session = "", json = nil)
+    headers = HTTP::Headers.new
+    if moodle_session != ""
+      headers.add("Cookie", "MoodleSession=" + moodle_session)
+    end
+    if json
+      headers.add("Content-Type", "application/json")
+    end
+
+    return HTTP::Client.post(url, headers: headers, form: data)
+  end
+
+  private def http_get(url, moodle_session)
+    headers = HTTP::Headers.new
+    headers.add("Cookie", "MoodleSession=" + moodle_session.to_s)
+    return HTTP::Client.get(url, headers: headers)
+  end
+end
